423a263b7dea261b41e743904c914ea1141d9dfa
Wagner Leonardi <leonardiwagner@gmail.com>
2019-12-17 16:53:07 -0300

    improve database reading (#3)

--- pedidoRepository.js
+++ pedidoRepository.js
@@ -1,10 +1,49 @@
 module.exports = function pedidoRepository(connection) {
   const get = async() => {
-    return await connection.query("SELECT TOP 50 número, data, hora FROM pedidos GROUP BY número,data, hora ORDER BY número DESC")
+    const query = `SELECT TOP 50 número, data, hora 
+                   FROM pedidos
+                   GROUP BY número, data, hora
+                   ORDER BY número DESC`
+
+    return await connection.query(query)
   }
 
   const getProdutos = async(numero) => {
-    return await connection.query(`SELECT número, produto, lançamento, cliente, vendedor, qte, valor, desconto, acrescimo, data, FormaPagto, hora, obs, cupom, DatadeEntrega, comissao, TipoPedido, StatusPedido FROM pedidos WHERE número = ${numero}`)
+    const query = `SELECT pe.número, pe.produto, pe.lançamento, pe.cliente,
+                          pe.vendedor, pe.qte, pe.valor, pe.desconto, pe.acrescimo, pe.data,
+                          pe.FormaPagto, pe.hora, pe.obs, pe.cupom, pe.DatadeEntrega, pe.comissao,
+                          pe.TipoPedido, pe.StatusPedido,
+                          pr.estoque_descrição
+                   FROM pedidos pe
+                   LEFT JOIN produtos pr ON pe.produto = pr.estoque_codigo
+                   WHERE número = ${numero}
+                   ORDER BY lançamento`
+
+    // first item is always the "pedido" summary, other items are the "produtos" from that "pedido"
+    const [pedido, ...produtos] = await connection.query(query)
+    const result = {
+      id: pedido['número'],
+      cliente: {
+        id: pedido['cliente']
+      },
+      vendedor: {
+        id: pedido['vendedor']
+      },
+      quantidade: pedido['qte'],
+      valor: pedido['valor'],
+      data: pedido['data'],
+      hora: pedido['hora'],
+      formaPagamento: pedido['FormaPagto'],
+      observacao: pedido['obs'],
+      produtos: produtos.map((produto) => ({
+        id: produto['produto'],
+        quantidade: produto['qte'],
+        valor: produto['valor'],
+        descricao: produto['estoque_descrição']
+      }))
+    }
+
+    return result
   }
 
   return {get, getProdutos}
