cba88f27224cf0fcb6e44bb4dbf2fd44befd5ff5
Gisele <42362130+giseleml@users.noreply.github.com>
2019-12-17 17:56:58 -0300

    4 pedidos info (#5)
    * Querying pedidos by their number
    
    * #4 Getting more pedido info

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.listen(3065, async() => {
   const connection = await database.connect()
   const pedidoRepository = PedidoRepository(connection)
   PedidoRoutes(app, pedidoRepository)
+
 })
 
 /*
 SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'
 const result = await connection.query("SELECT TOP 5 número, produto, lançamento, cliente, vendedor, qte, valor, desconto, acrescimo, data, FormaPagto, hora, obs, cupom, DatadeEntrega, comissao, TipoPedido, StatusPedido FROM pedidos")
 const result = await connection.query("SELECT TOP 5 estoque_codigo, estoque_referência, estoque_descrição, estoque_CodBarras, estoque_tabela, estoque_ativo FROM produtos")
-
 */
